8834227442703dccbd3f697bff489e79ba685017
Eduardo Bernardino <eduardomoura.moura@gmail.com>
2022-02-22 07:16:48 -0300

    refactor: refatoração de códigos melhorando a estrutura dos testes

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -5,16 +5,20 @@ import TestUtil from '../../src/common/test/TestUtil';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
+const user = TestUtil.getValidUser();
+
 describe('UserService', () => {
   let service: UserService;
 
   const mockRepository = {
-    find: jest.fn(),
-    findOne: jest.fn(),
-    create: jest.fn(),
-    save: jest.fn(),
-    update: jest.fn(),
-    remove: jest.fn(),
+    find: jest.fn().mockReturnValue([user, user]),
+    findOne: jest.fn().mockReturnValue(user),
+    create: jest.fn().mockReturnValue(user),
+    save: jest.fn().mockReturnValue(user),
+    update: jest.fn().mockReturnValue({
+      affected: 1,
+    }),
+    remove: jest.fn().mockReturnValue(user),
   };
 
   beforeAll(async () => {
@@ -31,25 +35,16 @@ describe('UserService', () => {
     service = module.get<UserService>(UserService);
   });
 
-  beforeEach(() => {
-    mockRepository.find.mockReset();
-    mockRepository.findOne.mockReset();
-    mockRepository.create.mockReset();
-    mockRepository.save.mockReset();
-    mockRepository.update.mockReset();
-    mockRepository.remove.mockReset();
-  });
-
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('findAll', () => {
     it('should be list all users', async () => {
-      const user = TestUtil.getValidUser();
-
-      mockRepository.find.mockReturnValue([user, user]);
-
       const users = await service.findAll();
 
       expect(users).toHaveLength(2);
@@ -59,7 +54,7 @@ describe('UserService', () => {
 
   describe('findOne', () => {
     it('should return a exception when does not to find a user', async () => {
-      mockRepository.findOne.mockReturnValue(null);
+      jest.spyOn(mockRepository, 'findOne').mockReturnValueOnce(null);
 
       expect(
         service.findById('4378b226-90b5-11ec-b909-0242ac120003'),
@@ -68,10 +63,6 @@ describe('UserService', () => {
     });
 
     it('should find a existing user', async () => {
-      const user = TestUtil.getValidUser();
-
-      mockRepository.findOne.mockReturnValue(user);
-
       const foundUser = await service.findById(
         '4378b226-90b5-11ec-b909-0242ac120002',
       );
@@ -88,19 +79,12 @@ describe('UserService', () => {
 
   describe('create', () => {
     it('should return a exception when user already exists', async () => {
-      const user = TestUtil.getValidUser();
-
-      mockRepository.findOne.mockReturnValue(user);
-
       expect(service.create(user)).rejects.toBeInstanceOf(ConflictException);
       expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
     });
 
     it('should create a user', async () => {
-      const user = TestUtil.getValidUser();
-
-      mockRepository.create.mockReturnValue(user);
-      mockRepository.save.mockReturnValue(user);
+      jest.spyOn(mockRepository, 'findOne').mockReturnValueOnce(null);
 
       const savedUser = await service.create(user);
 
@@ -112,9 +96,7 @@ describe('UserService', () => {
 
   describe('update', () => {
     it('should return a exception when does not to find a user', async () => {
-      const user = TestUtil.getValidUser();
-
-      mockRepository.findOne.mockReturnValue(null);
+      jest.spyOn(mockRepository, 'findOne').mockReturnValueOnce(null);
 
       expect(
         service.update({
@@ -128,13 +110,6 @@ describe('UserService', () => {
     });
 
     it('should update a user', async () => {
-      const user = TestUtil.getValidUser();
-
-      mockRepository.findOne.mockReturnValue(user);
-      mockRepository.update.mockReturnValue({
-        affected: 1,
-      });
-
       const updatedUser = await service.update({
         userId: user.id,
         name: user.name,
@@ -149,7 +124,7 @@ describe('UserService', () => {
 
   describe('delete', () => {
     it('should return a exception when does not to find a user', async () => {
-      mockRepository.findOne.mockReturnValue(null);
+      jest.spyOn(mockRepository, 'findOne').mockReturnValueOnce(null);
 
       expect(
         service.delete('4378b226-90b5-11ec-b909-0242ac120003'),
@@ -158,11 +133,6 @@ describe('UserService', () => {
     });
 
     it('should delete a user', async () => {
-      const user = TestUtil.getValidUser();
-
-      mockRepository.findOne.mockReturnValue(user);
-      mockRepository.remove.mockReturnValue(user);
-
       const removeUser = await service.delete(
         '4378b226-90b5-11ec-b909-0242ac120002',
       );
